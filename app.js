2a1da2371bef4abe04b6994729c6d35aeafeb57e
Stan <skukhniy@gmail.com>
2022-07-27 14:42:51 -0400

    finished project

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/new");
+var formRouter = require("./routes/form");
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/new", formRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,18 @@ router.get("/", function (req, res, next) {
 	res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+// recieve post from /new form
+router.post("/new", (req, res, next) => {
+	// grab input values from the form field
+	const newMessage = {
+		text: req.body.message_field,
+		user: req.body.user_field,
+		added: new Date(),
+	};
+	// push new message to messages array
+	messages.push(newMessage);
+	// send users back to homepage
+	res.redirect("/");
+});
+
 module.exports = router;
